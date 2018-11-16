23610d608e5838bc9789a92d13771a4536dd26a4
Arun Sivakumar <arun.s@circles.asia>
2018-11-17 01:04:44 +0800

    Initial Commit

---
+++ SystemSettings/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+// Created by Arun Sivakumar on 17/11/18.
+// Copyright © 2018 Arun Sivakumar. All rights reserved.
+// 
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SystemSettings/ViewController.swift
@@ -0,0 +1,24 @@
+//
+// Created by Arun Sivakumar on 17/11/18.
+// Copyright © 2018 Arun Sivakumar. All rights reserved.
+// 
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
